762f3f1c1c6a3bf73663944fcfb230a3c199d562
GondekNP <gonde012@umn.edu>
2016-08-23 12:33:34 -0500

    Delete comp.R

--- comp.R
+++
@@ -1,7 +0,0 @@
-library(devtools)
-source_url('https://drive.google.com/open?id=0B5SLx6M1A7DgYjM1MVVJYk80Nk0')
-
-getState <- function(){environment(sys.function(1))$e}
-lt=getState()$log$user
-print(ver(lt, 1))
-
