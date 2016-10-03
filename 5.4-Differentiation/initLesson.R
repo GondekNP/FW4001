b860d9d3799e83baa34aacc2f26e4686d16b9e15
jfieberg <jfieberg@umn.edu>
2016-10-02 22:49:58 -0500

    still trying to fix 5.4

--- 5.4-Differentiation/initLesson.R
+++ 5.4-Differentiation/initLesson.R
@@ -2,7 +2,7 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-#setwd("~/Documents/FW4001/5.4-Differentiation")
+# setwd("~/Documents/FW4001/5.4-Differentiation")
 swirl_options(swirl_logging = TRUE)
 suppressMessages(library(Lock5Data))
 suppressMessages(library(mosaic))
