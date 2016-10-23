932fbbcb1ada1503ffbb597631361f9da9f985e8
John R Fieberg <jfieberg@ad.umn.edu>
2016-10-23 16:33:29 -0500

    attempt to fix 9.2

--- 9.2-Regression.and.prediction/rattle2.R
+++ 9.2-Regression.and.prediction/rattle2.R
@@ -2,5 +2,5 @@ suppressMessages(library(abd))
 data("Rattlesnakes")
 invisible(Rattlesnakes)
 Rattlesnakes
-snake.lm<-lm(temp.change ~ meal.size, data = Rattlesnakes))
+snake.lm<-lm(temp.change ~ meal.size, data = Rattlesnakes)
 set.seed(1231)
