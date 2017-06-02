3877443896080e4ffa97f3e31fd0167065d5f1a8
John R Fieberg <jfieberg@ad.umn.edu>
2017-06-02 16:40:31 -0500

    Update lesson.yaml to include google form submission

--- DataSummary.R
+++ DataSummary.R
@@ -1,8 +1,12 @@
 #' Load libraries
+library(googlesheets)
 library(dplyr)
+mydat<-gs_title("Swirl2017 (Responses)" )
+swirlresults<-gs_read_csv(mydat)
+write.csv(swirlresults, file="temp.csv", row.names = FALSE)
 
 #' Select the swirl csv file
-temp<-swirlify::google_form_decode()
+temp<-swirlify::google_form_decode("temp.csv")
 
 #' Process statistics by user
 temp%>%group_by(user)%>%summarize(propcorrect=mean(correct, na.rm=TRUE), num.q=length(unique(question_number)))
