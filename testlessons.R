7f62fdfc899e9d0dcfca65134e6f675882c8f7a0
John R Fieberg <jfieberg@ad.umn.edu>
2017-06-02 21:45:40 -0500

    fix yamls last line

--- testlessons.R
+++ testlessons.R
@@ -1,5 +1,11 @@
 library(swirlify)
 
 # http://swirlstats.com/instructors.html
-set_lesson("2.1-Types.of.variables/lesson.yaml") # Set the lesson directly
+set_lesson("3.1-Boxplots/lesson.yaml") # Set the lesson directly
 demo_lesson()
+
+#  I'm on 3.1...
+
+
+uninstall_all_courses()
+install_course_github("GondekNP", course_name = "FW4001")
