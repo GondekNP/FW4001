a51bf399cc486d96a515d6464b8d9283df619792
John R Fieberg <jfieberg@ad.umn.edu>
2017-06-02 22:37:01 -0500

    add code to 5.3 (from earlier 5 lesson)

--- 5.3-Bootstraps.vs.randomization/caffeine.R
+++ 5.3-Bootstraps.vs.randomization/caffeine.R
@@ -1,3 +1,4 @@
 suppressMessages(library(Lock5Data))
 data("CaffeineTaps")
 invisible(CaffeineTaps)
+RandDist <- do(1000)*{diffmean(Taps~shuffle(Group), data=CaffeineTaps)}
--- testlessons.R
+++ testlessons.R
@@ -1,11 +1,13 @@
 library(swirlify)
 
 # http://swirlstats.com/instructors.html
-set_lesson("3.1-Boxplots/lesson.yaml") # Set the lesson directly
+set_lesson("3.3-Sample.and.resample/lesson.yaml") # Set the lesson directly
 demo_lesson()
 
 #  I'm on 3.1...
 
-
-uninstall_all_courses()
+# cntrl + shift+f10
+install.packages("swirl")
+library(swirl)
+?uninstall_all_courses(force = FALSE)
 install_course_github("GondekNP", course_name = "FW4001")
