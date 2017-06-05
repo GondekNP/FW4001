2ba6f752819319a94af333c06a026bc88e71bc29
John R Fieberg <jfieberg@ad.umn.edu>
2017-06-04 21:14:19 -0500

    making 4.2 and 4.3 stand alone.

--- 4.2-Std.err.and.confints/caffeine.R
+++ 4.2-Std.err.and.confints/caffeine.R
@@ -1,3 +1,4 @@
 suppressMessages(library(Lock5Data))
 data("CaffeineTaps")
 invisible(CaffeineTaps)
+CaffBoot <- do(1000)*{mean(~Taps, data=resample(CaffGroup))}
--- 4.3-Bootstrap.interpretation/caffeine.R
+++ 4.3-Bootstrap.interpretation/caffeine.R
@@ -1,3 +1,9 @@
 suppressMessages(library(Lock5Data))
 data("CaffeineTaps")
 invisible(CaffeineTaps)
+CaffBoot <- do(1000)*{mean(~Taps, data=resample(CaffGroup))}
+CaffMean <- mean(~Taps, data=CaffGroup)
+CaffSD <- sd(~result, data=CaffBoot)
+Hi <- CaffMean + 2 * CaffSD
+Lo <- CaffMean - 2 * CaffSD
+
