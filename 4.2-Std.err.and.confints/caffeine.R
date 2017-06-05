36f8a092ba19d38600d264e033b82049e1111c5e
John R Fieberg <jfieberg@ad.umn.edu>
2017-06-04 21:21:38 -0500

    fixing 4.2, 4.3

--- 4.2-Std.err.and.confints/caffeine.R
+++ 4.2-Std.err.and.confints/caffeine.R
@@ -1,4 +1,5 @@
 suppressMessages(library(Lock5Data))
 data("CaffeineTaps")
 invisible(CaffeineTaps)
+CaffGroup <- filter(CaffeineTaps,Group=="Caffeine")
 CaffBoot <- do(1000)*{mean(~Taps, data=resample(CaffGroup))}
--- 4.3-Bootstrap.interpretation/caffeine.R
+++ 4.3-Bootstrap.interpretation/caffeine.R
@@ -1,6 +1,7 @@
 suppressMessages(library(Lock5Data))
 data("CaffeineTaps")
 invisible(CaffeineTaps)
+CaffGroup <- filter(CaffeineTaps,Group=="Caffeine")
 CaffBoot <- do(1000)*{mean(~Taps, data=resample(CaffGroup))}
 CaffMean <- mean(~Taps, data=CaffGroup)
 CaffSD <- sd(~result, data=CaffBoot)
