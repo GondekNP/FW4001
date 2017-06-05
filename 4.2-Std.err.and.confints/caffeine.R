28490e8f964738604cb6f05df0b4c6e5cbd9d2ec
John R Fieberg <jfieberg@ad.umn.edu>
2017-06-04 21:37:52 -0500

    not sure why caffboot not working

--- 4.2-Std.err.and.confints/caffeine.R
+++ 4.2-Std.err.and.confints/caffeine.R
@@ -1,3 +1,6 @@
 suppressMessages(library(Lock5Data))
 data("CaffeineTaps")
 invisible(CaffeineTaps)
+CaffGroup <- filter(CaffeineTaps,Group=="Caffeine")
+CaffBoot <- do(1000)*{mean(~Taps, data=resample(CaffGroup))}
+
